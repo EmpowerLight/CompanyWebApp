6e5e28d7677f824918035d2d31c907a52b10a430
EmpowerLight <ankit.shrestha196@gmail.com>
2024-04-19 13:51:22 +0545

    Add missing files

---
+++ CompanyWebApp/Models/WholeCompanyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompanyWebApp.Models
+{
+    public class WholeCompanyModel
+    {
+        public CompanyModel Company { get; set; }
+
+        public CompanyInfoModel CompanyInfo { get; set; }
+    }
+}
